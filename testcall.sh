7cd309f4f96abe0f84b260496c793424a6cd51bc
nephros <nemo@pgxperiiia10>
2023-03-28 17:29:47 +0200

    make challenge harder with escaped characters

--- testcall.sh
+++ testcall.sh
@@ -5,7 +5,7 @@ path="/com/jolla/settings/ui"
 iface="com.jolla.settings.ui"
 method=showAddNetworkDialogFromQR
 
-options='wifi:S:MY-Access-Point;T:WPA2-EAP;P:AVerySecretPassword;H:false;I:my-identity;A:anon-identity;PH2:MSCHAPv2;;'
+options='wifi:S:MY-Access-Point;T:WPA2-EAP;P:Secr\;et\"Password\"\;;H:false;I:my-identity;A:anon-identity;PH2:MSCHAPv2;;'
 
 echo busctl call $service $path $iface ${method} "s" "$options"
 busctl --user call $service $path $iface ${method} "s" "$options"
